0d3e8adacb639105af184c6ca57f0f1f2dff93f4
Cauã <cauakathdev@gmail.com>
2021-05-27 11:32:50 -0300

    Instância de duas pessoas

--- src/main/java/br/com/senai/api/controller/PessoaController.java
+++ src/main/java/br/com/senai/api/controller/PessoaController.java
@@ -1,13 +1,31 @@
 package br.com.senai.api.controller;
 
+import br.com.senai.domain.model.Pessoa;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class PessoaController {
 
     @GetMapping("/pessoas")
-    public String listar() {
-        return "Cauã lindo Ok";
+    public List<Pessoa> listar() {
+        Pessoa pessoa1 = new Pessoa(
+                1L,
+                "Cauã",
+                "caua@gmail.com",
+                "(47)98829-1291"
+        );
+
+        Pessoa pessoa2 = new Pessoa(
+                2L,
+                "Miriam",
+                "miriam@gmail.com",
+                "(47)92932-2321"
+        );
+
+        return Arrays.asList(pessoa1, pessoa2);
     }
 }
