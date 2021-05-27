abec7be351e1b622d17818766af5869871493072
Cauã <cauakathdev@gmail.com>
2021-05-27 11:34:37 -0300

    Criação das migrations de Create Table e Insert

---
+++ src/main/resources/db/migration/V0002__insertPessoa.sql
@@ -0,0 +1,5 @@
+INSERT INTO pessoa
+VALUES (null, "Cauã", "caua@gmail.com", "(47)97263-2732"),
+(null, "Fran", "fran@gmail.com", "(47)92721-1312"),
+(null, "Pedro", "pedro@outlook.com", "(47)97271-2636"),
+(null, "Antônio", "antonio@bol.com.br", "(47)97862-2232");
