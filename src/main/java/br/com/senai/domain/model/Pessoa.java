02b9f0a8010e33560df0f888179068aece164802
Cauã <cauakathdev@gmail.com>
2021-05-31 09:49:09 -0300

    Adição de parâmeros de verificação de dados

--- src/main/java/br/com/senai/domain/model/Pessoa.java
+++ src/main/java/br/com/senai/domain/model/Pessoa.java
@@ -4,11 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.FieldDefaults;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import static lombok.AccessLevel.PRIVATE;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@FieldDefaults(level = PRIVATE)
 @Getter
 @Setter
 @Entity
@@ -16,10 +26,18 @@ public class Pessoa {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    long id;
 
-    private String nome;
-    private String email;
-    private String telefone;
+    @NotBlank
+    @Size(max = 80)
+    String nome;
 
+    @NotBlank
+    @Email
+    @Size(min = 5)
+    String email;
+
+    @NotBlank
+    @Size(min = 14, max = 14)
+    String telefone;
 }
