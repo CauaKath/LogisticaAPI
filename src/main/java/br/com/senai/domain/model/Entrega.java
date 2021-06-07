cb0cc3a05708210578219448ce5143ff9836050d
Cauã <cauakathdev@gmail.com>
2021-06-07 11:41:56 -0300

    Model com os atributos da Entrega

---
+++ src/main/java/br/com/senai/domain/model/Entrega.java
@@ -0,0 +1,40 @@
+package br.com.senai.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "entregas")
+public class Entrega {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "pessoa_id")
+    private Pessoa pessoa;
+
+    @Embedded
+    private Destinatario destinatario;
+
+    private BigDecimal taxa;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @Enumerated(EnumType.STRING)
+    private StatusEntrega status;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private LocalDateTime data_pedido;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private LocalDateTime data_finalizacao;
+
+}
