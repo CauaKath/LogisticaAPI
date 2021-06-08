cc0d9f69dea81844e670e870c4ccfb8e95ccad24
Cauã <cauakathdev@gmail.com>
2021-06-08 15:20:56 -0300

    Adição dos métodos de busca e busca por id

--- src/main/java/br/com/senai/api/controller/EntregaController.java
+++ src/main/java/br/com/senai/api/controller/EntregaController.java
@@ -1,10 +1,14 @@
 package br.com.senai.api.controller;
 
 import br.com.senai.domain.model.Entrega;
+import br.com.senai.domain.repository.EntregaRepository;
 import br.com.senai.domain.service.SolicitacaoEntregaService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -12,11 +16,22 @@ import org.springframework.web.bind.annotation.*;
 public class EntregaController {
 
     private SolicitacaoEntregaService solicitacaoEntregaService;
+    private EntregaRepository entregaRepository;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Entrega solicitar(@RequestBody Entrega entrega) {
         return solicitacaoEntregaService.solicitar(entrega);
+    }
+
+    @GetMapping
+    public List<Entrega> listar() {
+        return solicitacaoEntregaService.listar();
+    }
+
+    @GetMapping("/{entregaId}")
+    public ResponseEntity<Entrega> buscarPorId(@PathVariable long entregaId) {
+        return solicitacaoEntregaService.buscarPorId(entregaId);
     }
 
 }
