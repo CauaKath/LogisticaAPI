9eeb86f4611ceb14cac446ebe59e962ceb17a7b1
Cauã <cauakathdev@gmail.com>
2021-06-18 10:59:34 -0300

    Adição de métodos para finalizar entrega e registrar ocorrência

--- src/main/java/br/com/senai/domain/model/Entrega.java
+++ src/main/java/br/com/senai/domain/model/Entrega.java
@@ -1,6 +1,7 @@
 package br.com.senai.domain.model;
 
 import br.com.senai.domain.ValidationGroups;
+import br.com.senai.domain.exception.TrataException;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,6 +13,8 @@ import javax.validation.groups.ConvertGroup;
 import javax.validation.groups.Default;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Setter
 @Getter
@@ -39,6 +42,9 @@ public class Entrega {
     @NotNull
     private BigDecimal taxa;
 
+    @OneToMany(mappedBy = "entrega", cascade = CascadeType.ALL)
+    private List<Ocorrencia> ocorrencias = new ArrayList<>();
+
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     @Enumerated(EnumType.STRING)
     private StatusEntrega status;
@@ -48,5 +54,33 @@ public class Entrega {
 
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private LocalDateTime data_finalizacao;
+
+    public void finalizarEntrega() {
+        if (naoPodeSerFinalizada()) {
+            throw new TrataException("Entrega não pode ser finalizada");
+        }
+        setStatus(StatusEntrega.FINALIZADA);
+        setData_finalizacao(LocalDateTime.now());
+    }
+
+    public boolean podeSerFinalizada() {
+        return StatusEntrega.PENDENTE.equals(getStatus());
+    }
+
+    public boolean naoPodeSerFinalizada() {
+        return !podeSerFinalizada();
+    }
+
+    public Ocorrencia adicionarOcorrencia(String descricao) {
+        Ocorrencia ocorrencia = new Ocorrencia();
+
+        ocorrencia.setDescricao(descricao);
+        ocorrencia.setDataRegistro(LocalDateTime.now());
+        ocorrencia.setEntrega(this);
+
+        this.getOcorrencias().add(ocorrencia);
+
+        return ocorrencia;
+    }
 
 }
