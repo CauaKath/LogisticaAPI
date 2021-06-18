63777fd5cf7a35cde258745c8c7eb4dbf333e294
Cauã <cauakathdev@gmail.com>
2021-06-18 11:02:58 -0300

    Exception para tratar erros quando a entrega não for encontrada

---
+++ src/main/java/br/com/senai/domain/exception/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package br.com.senai.domain.exception;
+
+public class EntityNotFoundException extends TrataException{
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+
+}
