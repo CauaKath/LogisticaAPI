1ddb243e6e1b7102df8d2368e04cfd4df83b925e
Cauã <cauakathdev@gmail.com>
2021-06-23 16:38:30 -0300

    Classe que recebe os parametros das roles

---
+++ src/main/java/br/com/senai/api/model/input/RoleInputDTO.java
@@ -0,0 +1,17 @@
+package br.com.senai.api.model.input;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+public class RoleInputDTO {
+
+    @Id
+    @NotBlank
+    private String nomeRole;
+
+}
